f01bdaa98986a22a92fb36f111249df9fb3926c2
Aimi <aimimf20@gmail.com>
2024-08-21 17:47:35 +0800

    fixing bugs with templates

--- website/__init__.py
+++ website/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__, template_folder="templates")
     app.config["SECRET_KEY"] = "W3AR3TH3COD3RS"
     
     from .views import views 
