530c20c4fb66225da1d25649123f8b9471aa8426
Nanaur1 <amyfrosta246@gmail.com>
2024-08-21 17:22:13 +0800

    fixing the website

--- website/__init__.py
+++ website/__init__.py
@@ -4,4 +4,8 @@ def create_app():
     app = Flask(__name__)
     app.config["SECRET_KEY"] = "W3AR3TH3COD3RS"
     
+    from .views import views 
+    
+    app.register_blueprint(views, url_prefix = '/' )
+    
     return app
