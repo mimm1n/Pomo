b2636d373c17476c123d83658baa9aa8036cc1c1
Muhammad <1231102548@student.mmu.edu.my>
2024-08-22 00:31:11 +0800

    Added new pages (Modified base.html,init and auth).

--- website/__init__.py
+++ website/__init__.py
@@ -4,8 +4,9 @@ def create_app():
     app = Flask(__name__, template_folder="templates")
     app.config["SECRET_KEY"] = "W3AR3TH3COD3RS"
     
-    from .views import views 
+    from .views import views
+    from .auth import auth 
     
     app.register_blueprint(views, url_prefix = '/' )
-    
+    app.register_blueprint(auth, url_prefix = '/' )
     return app
--- website/auth.py
+++ website/auth.py
@@ -1,3 +1,23 @@
 from flask import Blueprint
 
 auth = Blueprint('auth', __name__ )
+
+
+@auth.route('/sign-up')
+def signUp():
+    return "<p>Testing No.1</p>"
+
+
+@auth.route('/customisation')
+def customisation():
+    return "<p>Testing No.2</p>"
+
+
+@auth.route('/settings')
+def settings():
+    return "<p>Testing No.3</p>"
+
+    
+@auth.route('/logout')
+def logout():
+    return "<p>Testing No.4</p>"
