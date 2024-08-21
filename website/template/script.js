366be9463c85e127e2f5ca9ca9fe598e8951a1fc
Aimi <aimimf20@gmail.com>
2024-08-21 14:08:28 +0800

    fixed a bit in sidebar nav

--- website/template/script.js
+++ website/template/script.js
@@ -1,4 +1,4 @@
 const toggler = document.querySelector(".toggler-btn");
-toggler.addEventListener("click", function() {
-    document.querySelector("#sidebar").classList.toggle("collapsed");
+toggler.addEventListener("click", function () {
+  document.querySelector("#sidebar").classList.toggle("collapsed");
 });
