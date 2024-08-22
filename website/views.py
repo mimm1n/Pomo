88d0f41fd7317f39275e71b3837cbb65a17fa456
Aimi <aimimf20@gmail.com>
2024-08-22 13:48:08 +0800

    fixed some files places

--- website/views.py
+++ website/views.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template
 
 views = Blueprint('views', __name__ )
 
-@views.route('/')
+@views.route('/', methods=["GET", "[POST]"])
 def home():
     return render_template('base.html')
  
