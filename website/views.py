a6ad08208fee81f82def1fcaafac4785b2eea559
Aimi <aimimf20@gmail.com>
2024-08-23 12:02:05 +0800

    fixed hrefs and routes

--- website/views.py
+++ website/views.py
@@ -5,5 +5,9 @@ views = Blueprint('views', __name__ )
 @views.route('/', methods=["GET", "[POST]"])
 def home():
     return render_template('base.html')
+
+@views.route('/settings')
+def settings():
+    return render_template('settings.html')
  
  
