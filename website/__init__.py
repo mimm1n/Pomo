820ff41211ab8d932b50654fd57f36796b3ca7d2
Muhammad <1231102548@student.mmu.edu.my>
2024-08-24 15:24:51 +0800

    Just want to update it with main.

--- website/__init__.py
+++ website/__init__.py
@@ -1,12 +1,11 @@
 from flask import Flask
 
 def create_app():
-    app = Flask(__name__, template_folder="templates")
+    app = Flask(__name__, template_folder="templates", static_folder="static")
     app.config["SECRET_KEY"] = "W3AR3TH3COD3RS"
     
-    from .views import views
-    from .auth import auth 
+    from .views import views 
     
     app.register_blueprint(views, url_prefix = '/' )
-    app.register_blueprint(auth, url_prefix = '/' )
+    
     return app
