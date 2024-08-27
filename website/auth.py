d8b311ed4449b0d990422fff2043cf9500939a94
Muhammad <1231102548@student.mmu.edu.my>
2024-08-27 21:57:33 +0800

    Want to merge with main.

--- website/auth.py
+++ website/auth.py
@@ -1,3 +1,8 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request
+
 
 auth = Blueprint('auth', __name__ )
+
+@auth.route('/sign-up')
+def sign_up():
+    return render_template("sign_up.html")
