2af94af9457d0d13d168171fe940ee7049559cde
Nanaur1 <amyfrosta246@gmail.com>
2024-08-30 09:20:38 +0800

    .

--- website/__init__.py
+++ website/__init__.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from os import path
+
 
 def create_app():
     app = Flask(__name__, template_folder="templates", static_folder="static")
@@ -10,4 +12,14 @@ def create_app():
     app.register_blueprint(views, url_prefix = '/' )
     app.register_blueprint(auth, url_prefix = '/' )
     
+    from .models import User
+    
+    create_database(app)
+    
     return app
+
+
+def create_database(app):
+    if not path.exists('website/' + DB_NAME):
+        db.create_all(app=app)
+        print('Created Database!')
