4c7c8b0aaad7d91165146aa8ff6b6edef11e5921
Aimi <aimimf20@gmail.com>
2024-09-10 00:16:31 +0800

    fixed minigame

---
+++ website/static/game.js
@@ -0,0 +1,35 @@
+//game.js//
+
+const dino = document.getElementById("dino");
+const cactus = document.getElementById("cactus"); // constant value
+const gameOver = document.getElementById("gameover");
+
+function jump() {
+  if (dino.classList != "jump") {
+    dino.classList.add("jump");
+
+    setTimeout(function () {
+      dino.classList.remove("jump");
+    }, 300);
+  }
+}
+
+let isAlive = setInterval(function () {
+  // get current dino Y position
+  let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
+
+  // get current cactus X position
+  let cactusLeft = parseInt(
+    window.getComputedStyle(cactus).getPropertyValue("left")
+  );
+
+  // detect collision
+  if (cactusLeft < 50 && cactusLeft > 0 && dinoTop >= 80) {
+    // collision
+    // alert("Game Over");
+  }
+}, 10);
+
+document.addEventListener("keydown", function (event) {
+  jump();
+});
