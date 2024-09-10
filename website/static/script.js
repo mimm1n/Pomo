eab892539d98656813b6d8ecae131dce26faa492
Aimi <aimimf20@gmail.com>
2024-09-10 13:58:35 +0800

    fixed todo list

--- website/static/script.js
+++ website/static/script.js
@@ -5,8 +5,6 @@ toggler.addEventListener("click", function () {
   document.querySelector("#sidebar").classList.toggle("collapsed");
 });
 
-
-
 // pomodoro
 
 let focusButton = document.getElementById("focus");
@@ -107,10 +105,42 @@ startButton.addEventListener("click", () => {
   }
 });
 
-const bgm = document.getElementById("bgm");
-let play = document.getElementById("play");
+// to-do list
 
-// (B2) CLICK TO PLAY
-play.onclick = () => {
-  bgm.play();
-};
+const inputTodo = document.getElementById("input-todo");
+const listContainer = document.getElementById("list-container");
+
+function addTask() {
+  if (inputTodo.value === "") {
+    alert("Write something!");
+  } else {
+    let li = document.createElement("li");
+    li.innerHTML = inputTodo.value;
+    listContainer.appendChild(li);
+    let span = document.createElement("Span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
+  }
+  inputTodo.value = "";
+  saveData();
+}
+
+listContainer.addEventListener("click", function (e) {
+  if (e.target.tagName === "LI") {
+    e.target.classList.toggle("checked");
+    saveData();
+  } else if (e.target.tagName === "SPAN") {
+    e.target.parentElement.remove();
+    saveData();
+  }
+}, false);
+
+function saveData(){
+  localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function showTask(){
+  listContainer.innerHTML = localStorage.getItem("data"); 
+}
+
+showTask();
