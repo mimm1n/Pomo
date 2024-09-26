0741d85915846faec5a83fd510f3f8a82f547981
Nanaur1 <amyfrosta246@gmail.com>
2024-09-26 19:05:15 +0800

    Fixed the game space jump issue

--- website/route.py
+++ website/route.py
@@ -6,15 +6,20 @@ main = Blueprint('main', __name__)
 
 @main.route('/profile', methods=['GET', 'POST'])
 def profile():
-
-    user = User.query.get(1)  #make it simple we use user id 1
-
+    
+    user = User.query.get(1) #getting the 1 id to simplyfy it
 
     if request.method == 'POST':
         new_email = request.form['email']
         new_username = request.form['username']
 
-        # Update user info
+        # Checks if the new username already exists 
+        existing_user = User.query.filter_by(username=new_username).first()
+        if existing_user and existing_user.id != user.id:
+            flash('Username already taken. Please choose another one.')
+            return redirect(url_for('main.profile'))
+
+        # if username is unique, it will update
         user.email = new_email
         user.username = new_username
         db.session.commit()
@@ -23,3 +28,4 @@ def profile():
         return redirect(url_for('main.profile'))
 
     return render_template('profile.html', user=user)
+
