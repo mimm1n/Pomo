573d9d64adc6e037fcd7b38825a1e019a9fa15b9
Aimi <aimimf20@gmail.com>
2024-09-26 19:15:17 +0800

    trying to do bg again

--- website/static/audio.js
+++ website/static/audio.js
@@ -27,14 +27,17 @@ checkbox.addEventListener("change", function () {
 
 // volbar
 
-// var volBar = document.querySelector("#volbar");
-
-// volBar.addEventListener("input", function (e) {
-//   bgm.volBar = e.currentTarget.value / 100;
-// });
-
 let volume = document.querySelector("#volbar");
 
 volume.addEventListener("change", function (e) {
   bgm.volume = e.currentTarget.value / 100;
 });
+
+// var bgArray = [
+//   '//placehold.it/400x300?text=BG1',
+//      '//placehold.it/400x300?text=BG2'
+//     ]
+// $('#dropdown-menu').on('change', function(){
+// value = $(this).val() - 1;
+// $('#test-bg').css({'background-image':'url(' + bgArray[value] + ')'});
+// });
