c66721a634ca393211ddc599eceb09b22adc31b8
Nanaur1 <amyfrosta246@gmail.com>
2024-09-26 23:51:49 +0800

    Placed route.py codes into auth to reduce the amount of files

--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, flash, redirect, url_for
+from flask import Blueprint, render_template, request, flash, redirect, url_for, flash
 from .models import User
 import re
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -124,6 +124,31 @@ def logout():
     logout_user()
     return redirect(url_for('auth.login'))
 
+
+@auth.route('/profile', methods=['GET', 'POST'])
+def profile_change():
+    
+    user = User.query.get(1) #getting the 1 id to simplyfy it
+
+    if request.method == 'POST':
+        new_email = request.form['email']
+        new_username = request.form['username']
+
+        # Checks if the new username already exists 
+        existing_user = User.query.filter_by(username=new_username).first()
+        if existing_user and existing_user.id != user.id:
+            flash('Username already taken. Please choose another one.')
+            return redirect(url_for('main.profile'))
+
+        # if username is unique, it will update
+        user.email = new_email
+        user.username = new_username
+        db.session.commit()
+
+        flash('Profile updated successfully!')
+        return redirect(url_for('main.profile'))
+
+    return render_template('profile.html', user=user)
 
 
 
